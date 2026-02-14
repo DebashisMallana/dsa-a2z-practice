9d68c4076641c8432b09e9d8951f97bc201fd7fa
DebashisMallana <debashismallana12@gmail.com>
2026-02-14 13:36:43 +0530

    Binary Search

--- binarysearch.java
+++ binarysearch.java
@@ -1,11 +1,12 @@
 //Day 1 of writing a single line of code i.e 08/02/2026
 public class binarysearch {
-    void search(int arr[])// day 2 of writing a single line of code i.e 09/02/2026
+    void search(int arr[], int key)// day 2 of writing a single line of code i.e 09/02/2026
     {
         //day 3 of writing a single line of code (10/02/2026)
         int l=arr.length;
         int start=0;//day 5 (12/02/2026)
-        int end=length-1;//day 6 (13/02/2026)
+        int end=l-1;//day 6 (13/02/2026)
+        int mid=(start+end)/2;//day 7(14/02/2026)
     }
     void recursivesearch(int arr[]){ //day 4 of writing a single line of code (11/02/2026)
 
