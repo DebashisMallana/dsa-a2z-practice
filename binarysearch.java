6832671823e0d553ee3afb322337627c97b34421
DebashisMallana <debashismallana12@gmail.com>
2026-02-18 21:06:57 +0530

    Binary Search

--- binarysearch.java
+++ binarysearch.java
@@ -10,7 +10,7 @@ public class binarysearch {
         while (start<=end) { //day 8(15/2/2026)
             if(a[mid]==key)// day 9(16/02/2026)
                 System.out.println("The element is found in the array");//day 10(17/02/2026)
-            
+            if(a[mid]<key)//day 11(18/02/2026)
             
         }
     }
