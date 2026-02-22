4a072062e0be4f6b382977978a0e01f9993871d8
DebashisMallana <debashismallana12@gmail.com>
2026-02-22 18:06:34 +0530

    Binary Search

--- binarysearch.java
+++ binarysearch.java
@@ -1,15 +1,15 @@
 //Day 1 of writing a single line of code i.e 08/02/2026
 public class binarysearch {
-    void search(int arr[], int key)// day 2 of writing a single line of code i.e 09/02/2026
+    void search(int a[], int key)// day 2 of writing a single line of code i.e 09/02/2026
     {
         //day 3 of writing a single line of code (10/02/2026)
-        int l=arr.length;
+        int l=a.length;
         int start=0;//day 5 (12/02/2026)
         int end=l-1;//day 6 (13/02/2026)
-        int mid=(start+end)/2;//day 7(14/02/2026)
-        while (start<=end) { //day 8(15/2/2026)
+        while (start<=end) {       
+        int mid=(start+end)/2;//day 8(15/2/2026)
             if(a[mid]==key)// day 9(16/02/2026)
-                System.out.println("The element is found in the array");//day 10(17/02/2026)
+                System.out.println("The element is found in the array in the index:"+mid);//day 10(17/02/2026)
             if(a[mid]<key)//day 11(18/02/2026)
         {
             end=mid-1;//day 12(19/02/2026)
@@ -18,10 +18,10 @@ public class binarysearch {
         {
             start=mid+1;//day 14(21/02/2026)
         }
-            
-        }
-    }
-    void recursivesearch(int arr[]){ //day 4 of writing a single line of code (11/02/2026)
+    }          
+     }
+    void recursivesearch(int a[], int key){ //day 4 of writing a single line of code (11/02/2026)
+        int l=a.length;//day 15(22/02/2026)
 
     }
 
