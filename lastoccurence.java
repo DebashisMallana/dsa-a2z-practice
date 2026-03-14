66926e6f48155162fb3ced02fb2f7720b470668b
DebashisMallana <debashismallana12@gmail.com>
2026-03-14 22:13:41 +0530

    first and last occurence of a target in a sorted array

--- lastoccurence.java
+++ lastoccurence.java
@@ -1,4 +1,57 @@
 public class lastoccurence {
+    //Given a sorted array of N integers, write a program to find the index of the last occurrence of the target key.
+    //  If the target is not found then return -1. Note: Consider 0 based indexing
     void lo(int a[],int key)
-    
-}
+    {
+        int l=a.length;
+        int start=0;
+        int end=l-1;
+        int occ=-1;;
+        while (start<=end) {       
+        int mid=(start+end)/2;
+            if(a[mid]==key)
+                {
+                  occ=mid;
+                } 
+              else if( a[mid]<key)
+        {
+           start=mid+1;
+        }  
+        else{
+            end=mid-1;    
+        }
+        }  
+        System.out.println("The last occurence of the element "+key+" is" +occ);
+    }
+    void fo(int a[],int key)
+    {
+        int l=a.length;
+        int start=0;
+        int end=l-1;
+        while (start<=end) {       
+        int mid=(start+end)/2;
+        if(a[mid]==key)
+                {
+                  System.out.println("The first occurence of the element "+key+" is" +mid);
+                  break;
+                } 
+              else if( a[mid]<key)
+        {
+           start=mid+1;
+        }  
+        else{
+            end=mid-1;    
+        }
+        }  
+       
+    }    
+
+      public static void main(String[] args) {
+       lastoccurence ob =new lastoccurence();
+        int arr[]={3, 4, 13, 13, 13, 20, 40} ;
+        ob.lo(arr,13);
+        ob.fo(arr,13);
+      }
+    }
+
+
