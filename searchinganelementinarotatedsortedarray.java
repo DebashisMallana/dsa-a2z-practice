d52f98b51bb86a9aaac25f40322281db34a8fdd5
DebashisMallana <debashismallana12@gmail.com>
2026-04-01 21:53:14 +0530

    searching an element in a rotated sorted array

--- searchinganelementinarotatedsortedarray.java
+++ searchinganelementinarotatedsortedarray.java
@@ -3,8 +3,7 @@ public class searchinganelementinarotatedsortedarray {
         int l=a.length;       
         int start=0;
         int end=l-1;
-        int i=1;
-        while(start<=end && a[i-1]<a[i]){
+        while(start<=end){
             int mid=(start+end)/2;
             if(a[mid]==key){
                 System.out.println("the key is at"+mid);
@@ -16,26 +15,13 @@ public class searchinganelementinarotatedsortedarray {
                 start=mid+1;
 
         }
-        int first=i;
-        int last=l-1;
-         while(first<=last){
-            int mid=(first+last)/2;
-            if(a[mid]==key){
-                    System.out.println("the key is at"+mid);
-            return ; }
-            else if (a[mid]>key)
-            last=mid-1;
-            else
-                first=mid+1;
-
-        }
         System.out.println("-1");
 
     }
     public static void main(String[] args) {
         int arr[]={4, 5, 6, 7, 0, 1, 2};
         searchinganelementinarotatedsortedarray ob= new searchinganelementinarotatedsortedarray();
-        ob.search(arr,1);
+        ob.search(arr,2);
     }
     
 }
