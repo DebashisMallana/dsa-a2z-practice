81a717bdac3518f282e16ddda4e3f4dbba118c30
DebashisMallana <debashismallana12@gmail.com>
2026-04-03 20:37:40 +0530

    searching an element in the rotated sorted array using modified binary search

--- searchinganelementinarotatedsortedarray.java
+++ searchinganelementinarotatedsortedarray.java
@@ -5,28 +5,32 @@ public class searchinganelementinarotatedsortedarray {
         int end=l-1;
         while(start<=end){
             int mid=(start+end)/2;
-            if(a[start]<=a[mid]){
             if(a[mid]==key){
                 System.out.println("the key is at"+mid);
             return;
             }
-            else if (a[mid]>key)
+           if(a[start]<=a[mid])//check if the left half is sorted
+           {
+            if(a[start]<=key && key<=a[mid])
                 end=mid-1;
             else
                 start=mid+1;
-
-        }
-        else
-            start=mid+1;
-        }
-            
+           }
+           else
+           {
+             if(a[mid]<=key && key<=a[end]) // checking if the element is present in the right half
+                 start=mid+1;
+            else              
+            end=mid-1;
+           }    
+        }      
         System.out.println("-1");
 
     }
     public static void main(String[] args) {
         int arr[]={4, 5, 6, 7, 0, 1, 2};
         searchinganelementinarotatedsortedarray ob= new searchinganelementinarotatedsortedarray();
-        ob.search(arr,2);
+        ob.search(arr,1);
     }
     
 }
