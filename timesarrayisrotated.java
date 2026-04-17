783cc51a2fa8523a8d629ccdb8265ee48f9b9d7f
DebashisMallana <debashismallana12@gmail.com>
2026-04-17 23:28:01 +0530

    finding the single element in a sorted array consisting of doubles

--- timesarrayisrotated.java
+++ timesarrayisrotated.java
@@ -1,26 +1,37 @@
 public class timesarrayisrotated {
-    public void find(int a[],int key)
+    public void find(int a[])
     {
         int l=a.length;       
         int start=0;
         int end=l-1;
-        while(start<=end){
-            int mid=(start+end)/2;
-            }
-           if(a[start]<=a[mid])//check if the left half is sorted
-           {
-           }
-           
-           else
-           {
-           }
-        System.out.println("-1");
+        int mini=Integer.MAX_VALUE;
+        int miniindex=-1;
+       while (start <= end) {
+        int mid = (start + end) / 2;
 
+        // left  half sorted 
+        if (a[start] <= a[mid]) {
+            if(a[start]<mini){
+                    mini=a[start];
+                    miniindex=start;
+            }
+                start=mid+1; 
+            }     
+            else{
+            if(a[mid]<mini){
+                mini=a[mid];
+                miniindex=mid;
+            }
+                end=mid-1;
+        }     
     }
+     System.out.println("The array is rotated "+miniindex+" times");
+}
+
     public static void main(String[] args) {
-        int arr[]={4, 5, 6, 7, 0, 1, 2};
+        int arr[]={3,4,5,1,2};
         timesarrayisrotated ob =new timesarrayisrotated();
-        ob.find(arr,1);
+        ob.find(arr);
     }
     
 }
