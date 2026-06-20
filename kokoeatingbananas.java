b777a1ecfa5798144f4c07dd1a26471c888fb23c
DebashisMallana <debashismallana12@gmail.com>
2026-06-20 21:40:09 +0530

    Koko eating bananas

---
+++ kokoeatingbananas.java
@@ -0,0 +1,16 @@
+public class kokoeatingbananas {
+    public int bananas(int n,int a[] , int h)
+    {
+        int max=0;
+        for(int i=0;i<n;i++)
+        {
+            max=Math.max(max,a[i]);
+        }
+        int start=1,end=max;
+        while(start<=end)
+        {
+            int mid=(start+end)/2;
+            
+        }
+    }
+}
