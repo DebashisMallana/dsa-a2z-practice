6e0512af3e83c2a0d3a3b93fd50ccd8084400525
DebashisMallana <debashismallana12@gmail.com>
2026-06-21 23:05:11 +0530

    koko eating bananas

--- kokoeatingbananas.java
+++ kokoeatingbananas.java
@@ -1,4 +1,13 @@
 public class kokoeatingbananas {
+    public int sumoftime(int a[],int m)
+    {
+        int s=0;
+        for(int i=0;i<a.length;i++)
+        {
+             s=s+(a[i]%m);
+        }
+        return s;
+    }
     public int bananas(int n,int a[] , int h)
     {
         int max=0;
@@ -10,7 +19,22 @@ public class kokoeatingbananas {
         while(start<=end)
         {
             int mid=(start+end)/2;
+            if(sumoftime(a,mid)==h)
+                return mid;
+            else if (sumoftime(a, mid)<h)
+                start=mid+1;
+            else 
+                end=mid-1;
             
         }
+        return -1;
+    }
+    public static void main(String[] args) {
+        kokoeatingbananas obj = new kokoeatingbananas();
+        int arr[]={7, 15, 6, 3};
+        int n = 4;
+        int h=8;
+        System.out.println(obj.bananas(n,arr,h));
+        System.out.println((int)7%8);
     }
 }
