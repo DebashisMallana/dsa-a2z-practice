951f5c86cbead72361fee88ce2c404fa662d29ce
DebashisMallana <debashismallana12@gmail.com>
2026-09-07 07:26:23 +0530

    ship packages within d days

--- shippackageswithinddays.java
+++ shippackageswithinddays.java
@@ -1,18 +1,18 @@
 public class shippackageswithinddays {
    int dayscalculator(int capacity,int a[])
    {
-    int days=0;
-    int total_weights=a[0];;
-    for(int i=1;i<a.length;i++)
+    int days=1;
+    int total_weights=0;;
+    for(int i=0;i<a.length;i++)
     {
-        if(total_weights<capacity)
+        if(total_weights+a[i]<=capacity)
         {
-            total_weights+=i;
-            days++;
+           total_weights+=a[i];
         }
         else
         {
-            total_weights=0;
+            days+=1;
+            total_weights=a[i];
         }
     }
     return days;
@@ -29,19 +29,18 @@ public class shippackageswithinddays {
         {
             sum+=i;
         }
-       int start=max,end=sum,ans=-1;
+       int start=max,end=sum;
        while(start<=end)
        {
         int mid=start+(end-start)/2;
         if(dayscalculator(mid,a)<=d)
-        {
-           ans=mid;   
+        {  
             end=mid-1;          
         }
             else                
          start=mid+1;
        }
-       return ans;       
+       return start;       
     }
    
     public static void main(String[] args) {
