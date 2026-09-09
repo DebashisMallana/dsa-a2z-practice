d723d9a33993a8d66659f4eb3a8a646dcc5be2ef
DebashisMallana <debashismallana12@gmail.com>
2026-09-09 21:51:48 +0530

    kth missing positive number in o(log n) time complexity

--- kthmissingpositivenumber.java
+++ kthmissingpositivenumber.java
@@ -1,24 +1,22 @@
 public class kthmissingpositivenumber
 {
-    int checkdays(int a[],int d)
-   {
-       int start=max,end=sum;
-       while(start<=end)
-       {
-        int mid=start+(end-start)/2;
-        if(dayscalculator(mid,a)<=d)
-        {  
-            end=mid-1;          
+    static int findmissing(int a[], int k)
+    {
+        int low=0;
+        int high=a.length-1;
+        while(low<=high)
+        {
+            int mid=low+(high-low)/2;
+            int missing=a[mid]-(mid+1);
+            if(missing<=k) low=mid+1;
+            else high=mid-1;
         }
-            else                
-         start=mid+1;
-       }
-       return start;       
+        return k+high+1;
     }
      public static void main(String[] args) {
         int arr[]={4,7,9,10};
-        int k=4;
-        System.out.println(print(arr,k));
+        int k=1;
+        System.out.println(findmissing(arr,k));
         
     }
 }
