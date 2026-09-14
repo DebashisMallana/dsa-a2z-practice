1fa68d07722324229d2e91f5714462aa3322f116
DebashisMallana <debashismallana12@gmail.com>
2026-09-14 14:36:52 +0530

    Agressive cow problem solved using brute force method

---
+++ agressivecowsbruteforce.java
@@ -0,0 +1,40 @@
+import java.util.*;
+public class agressivecowsbruteforce {
+    static boolean canWePlace(int a[],int dist,int cows)
+    {
+        int CountCow=1;
+        int last=a[0];
+       for(int i=1;i<a.length;i++)
+       {
+          if(a[i]-last>=dist)
+          {
+            CountCow+=1;
+            last=a[i];
+          }      
+       }
+       if(CountCow>=cows)
+            return true;
+          else
+            return false;     
+       
+    }
+    static int agressivecows(int a[], int cows)
+    {
+        Arrays.sort(a);
+        int l=a.length;
+        int max=a[l-1];
+        int min=1;
+        for(int i=1;i<=max;i++)
+        {
+            if(canWePlace(a,i,cows)) continue;
+            else return i-1;
+        }
+        return -1;
+    }
+    public static void main(String args[])
+    {
+        int arr[]={4,2,1,3,6};// 1 2 3 4 6
+        int k=2;
+        System.out.println(agressivecows(arr,k));
+    }
+    }
--- kthmissingpositivenumber.java
+++ kthmissingpositivenumber.java
@@ -8,13 +8,13 @@ public class kthmissingpositivenumber
         {
             int mid=low+(high-low)/2;
             int missing=a[mid]-(mid+1);
-            if(missing<=k) low=mid+1;
+            if(missing<k) low=mid+1;
             else high=mid-1;
         }
         return k+high+1;
     }
      public static void main(String[] args) {
-        int arr[]={4,7,9,10};
+        int arr[]={2};
         int k=1;
         System.out.println(findmissing(arr,k));
         
